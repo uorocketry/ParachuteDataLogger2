1cb6539541968cfcba8bffd21f4895da519bd022
betato <battlestargatticus@gmail.com>
2021-10-27 17:49:17 -0400

    Switch to platformio

---
+++ lib/scales/src/scales.h
@@ -0,0 +1,18 @@
+#ifndef SCALES_H
+#define SCALES_H
+
+namespace Scales
+{
+    struct Reading
+    {
+        Reading() : x(0.0f), y(0.0f), z(0.0f) {}
+        float x, y, z;
+    };
+
+    void init();
+    void setPower(bool on);
+    bool scalesReady();
+    Reading readOnce();
+}
+
+#endif
---
+++ src/loadcell/loadcell.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <scales.h>
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    Scales::init();
+    Serial.begin(115200);
+    delay(1000);
+    Scales::setPower(true);
+}
+
+void loop()
+{
+    delay(1000);
+    Scales::Reading reading = Scales::readOnce();
+
+    Serial.print("X: "); // Axial
+    Serial.println(reading.x);
+    Serial.print("Y: "); // Vertical
+    Serial.println(reading.y);
+    Serial.print("Z: "); // Horizontal
+    Serial.println(reading.z);
+    Serial.println("---");
+}
