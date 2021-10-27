655fa8e99eb2af48c4d5cfe174e9346ea90e7cae
betato <battlestargatticus@gmail.com>
2021-10-27 18:02:23 -0400

    Add anemometer board to project

---
+++ src/anemometer/anemometer.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+}
